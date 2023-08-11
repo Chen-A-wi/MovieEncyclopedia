5da6e6f3068cc8d71c4867dedba899830f980859
BenChen <tttw216@gmail.com>
2023-08-11 14:40:20 +0800

    [Feat] init koin

--- app/src/main/java/com/awilab/movieencyclopedia/common/App.kt
+++ app/src/main/java/com/awilab/movieencyclopedia/common/App.kt
@@ -8,6 +8,8 @@ import org.koin.core.context.startKoin
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        initKoin()
     }
 
     private fun initKoin() {

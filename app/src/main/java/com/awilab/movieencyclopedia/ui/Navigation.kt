a46d4e9f4a7c6b0a17f3a1f365aeb3f19f0e10d9
BenChen <tttw216@gmail.com>
2024-03-18 16:44:28 +0800

    [Feat] Navigation

---
+++ app/src/main/java/com/awilab/movieencyclopedia/ui/Navigation.kt
@@ -0,0 +1,11 @@
+package com.awilab.movieencyclopedia.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MainNavigation(
+    navController: NavController = rememberNavController(),
+) {
+}

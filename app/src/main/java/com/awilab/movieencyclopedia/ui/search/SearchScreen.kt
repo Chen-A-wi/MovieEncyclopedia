75c080d27d5be2d69415912264bd3a8cc2737e68
Ben <tttw216@gmail.com>
2024-05-06 22:20:47 +0800

    [Fix] add searchbar shape

--- app/src/main/java/com/awilab/movieencyclopedia/ui/search/SearchScreen.kt
+++ app/src/main/java/com/awilab/movieencyclopedia/ui/search/SearchScreen.kt
@@ -5,15 +5,19 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -25,6 +29,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -73,9 +78,14 @@ fun SearchField(
     onClear: () -> Unit,
 ) {
     TextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 8.dp),
+        modifier = Modifier.fillMaxWidth().border(
+            BorderStroke(
+                width = 4.dp,
+                brush = Brush.horizontalGradient(listOf(Color.Green, Color.Blue)),
+            ),
+            shape = RoundedCornerShape(50),
+        ),
+        shape = MaterialTheme.shapes.extraLarge,
         value = keyword,
         onValueChange = onValueChange,
         placeholder = { Text(text = stringResource(id = R.string.lab_search)) },

dd67aad6a5c6ab8beb445f4a2c82641b355197e9
BenChen <tttw216@gmail.com>
2024-05-07 14:03:27 +0800

    [Fix] change theme color

--- app/src/main/java/com/awilab/movieencyclopedia/ui/search/SearchScreen.kt
+++ app/src/main/java/com/awilab/movieencyclopedia/ui/search/SearchScreen.kt
@@ -5,12 +5,11 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
@@ -18,19 +17,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -46,26 +41,23 @@ fun SearchScreen(
     navController: NavController,
     vm: SearchViewModel = koinViewModel(),
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                modifier = Modifier.fillMaxWidth(),
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = Color.Green.copy(alpha = 0.3f),
-                ),
-                title = {
-                    SearchField(
-                        keyword = vm.keywordStateFlow.collectAsState(initial = "").value,
-                        onValueChange = vm::onSearch,
-                        onClear = vm::onClear,
-                    )
-                },
-            )
-        },
-    ) { innerPadding ->
+    Scaffold { innerPadding ->
         Column(
             modifier = Modifier.padding(innerPadding),
         ) {
+            Box(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxWidth()
+                    .background(color = MaterialTheme.colorScheme.primary),
+            ) {
+                SearchField(
+                    keyword = vm.keywordStateFlow.collectAsState(initial = "").value,
+                    onValueChange = vm::onSearch,
+                    onClear = vm::onClear,
+                )
+            }
+
             Text(text = "Hello Search Screen")
         }
     }
@@ -77,22 +69,17 @@ fun SearchField(
     onValueChange: (keyword: String) -> Unit,
     onClear: () -> Unit,
 ) {
-    TextField(
-        modifier = Modifier.fillMaxWidth().border(
-            BorderStroke(
-                width = 4.dp,
-                brush = Brush.horizontalGradient(listOf(Color.Green, Color.Blue)),
-            ),
-            shape = RoundedCornerShape(50),
-        ),
+    OutlinedTextField(
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp).fillMaxWidth(),
         shape = MaterialTheme.shapes.extraLarge,
         value = keyword,
         onValueChange = onValueChange,
-        placeholder = { Text(text = stringResource(id = R.string.lab_search)) },
         singleLine = true,
+        label = { Text(text = stringResource(id = R.string.lab_search)) },
         leadingIcon = {
             Icon(
                 Icons.Filled.Search,
+                tint = MaterialTheme.colorScheme.outline,
                 contentDescription = stringResource(id = R.string.lab_search),
             )
         },
@@ -107,15 +94,19 @@ fun SearchField(
                 ) {
                     Icon(
                         Icons.Filled.Clear,
+                        tint = MaterialTheme.colorScheme.outline,
                         contentDescription = stringResource(id = R.string.lab_clear),
                     )
                 }
             }
         },
-        colors = TextFieldDefaults.colors(
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent,
+        supportingText = { Text(text = "Supporting text") },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = MaterialTheme.colorScheme.outlineVariant,
+            unfocusedBorderColor = MaterialTheme.colorScheme.outline,
+            focusedLabelColor = MaterialTheme.colorScheme.outlineVariant,
+            unfocusedLabelColor = MaterialTheme.colorScheme.outline,
+            cursorColor = MaterialTheme.colorScheme.outline,
         ),
     )
 }
--- app/src/main/java/com/awilab/movieencyclopedia/ui/steam/SteamScreen.kt
+++ app/src/main/java/com/awilab/movieencyclopedia/ui/steam/SteamScreen.kt
@@ -2,14 +2,14 @@ package com.awilab.movieencyclopedia.ui.steam
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
@@ -24,9 +24,9 @@ fun SteamScreen(
 ) {
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
+            TopAppBar(
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = Color.Green.copy(alpha = 0.3f),
+                    containerColor = MaterialTheme.colorScheme.primary,
                 ),
                 title = { Text("SteamScreen") },
             )

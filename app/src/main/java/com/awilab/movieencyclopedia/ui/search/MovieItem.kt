9b5b45758ac72dd349f5a9e050422f54c8282cb3
BenChen <tttw216@gmail.com>
2024-05-23 16:52:58 +0800

    [Feat] MovieItem

---
+++ app/src/main/java/com/awilab/movieencyclopedia/ui/search/MovieItem.kt
@@ -0,0 +1,17 @@
+package com.awilab.movieencyclopedia.ui.search
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.awilab.movieencyclopedia.ui.theme.MovieEncyclopediaTheme
+
+@Composable
+fun MovieItem() {
+}
+
+@Preview
+@Composable
+fun MovieItemPreview() {
+    MovieEncyclopediaTheme {
+        MovieItem()
+    }
+}

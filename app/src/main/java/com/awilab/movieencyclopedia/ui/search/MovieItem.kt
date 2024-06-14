6704dffbe8c7c8120157ede33eea90cf7251e84a
BenChen <tttw216@gmail.com>
2024-06-14 15:56:04 +0800

    [Feat] item

--- app/src/main/java/com/awilab/movieencyclopedia/ui/search/MovieItem.kt
+++ app/src/main/java/com/awilab/movieencyclopedia/ui/search/MovieItem.kt
@@ -1,17 +1,59 @@
 package com.awilab.movieencyclopedia.ui.search
 
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
+import com.awilab.movieencyclopedia.R
 import com.awilab.movieencyclopedia.ui.theme.MovieEncyclopediaTheme
 
 @Composable
-fun MovieItem() {
+fun MovieItem(modifier: Modifier) {
+    ConstraintLayout {
+        val (imgId, titleId) = createRefs()
+
+        SubcomposeAsyncImage(
+            contentScale = ContentScale.FillHeight,
+            model = ImageRequest.Builder(LocalContext.current)
+                .crossfade(true)
+                .data("https://image.tmdb.org/t/p/original/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg")
+                .size(720, 360).build(),
+            contentDescription = stringResource(id = R.string.lab_search),
+            modifier = modifier.constrainAs(imgId) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            loading = {
+                CircularProgressIndicator()
+            },
+        )
+
+        Text(
+            text = "Movie Title",
+            modifier = modifier.constrainAs(titleId) {
+                top.linkTo(imgId.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+            },
+        )
+    }
 }
 
 @Preview
 @Composable
 fun MovieItemPreview() {
     MovieEncyclopediaTheme {
-        MovieItem()
+        MovieItem(
+            Modifier,
+        )
     }
 }
--- app/src/main/java/com/awilab/movieencyclopedia/ui/search/SearchScreen.kt
+++ app/src/main/java/com/awilab/movieencyclopedia/ui/search/SearchScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -23,8 +22,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -34,7 +31,6 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import org.koin.androidx.compose.koinViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Destination<RootGraph>()
 @Composable
 fun SearchScreen(
@@ -58,7 +54,10 @@ fun SearchScreen(
                 )
             }
 
-            Text(text = "Hello Search Screen")
+            MovieItem(
+                Modifier
+                    .padding(innerPadding),
+            )
         }
     }
 }
